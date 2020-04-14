68c50a1644eca8bc973445f0e23771ebaee2cdbb
Krzysztof Augustowski <krzysztof@augustowski.net>
2020-04-14 20:19:12 +0200

    jupyterlab in docker template fix (without batch for windows)

--- #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/README.md
+++ #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/README.md
@@ -6,4 +6,4 @@ Just execute `run-jupyter.sh` and follow the link printed in console
 If you're running windows and have linux docker containers enabled you can execute commands from `run-jupyter.sh` manually
 
 To export your new conda libraries execute following command in jupyter lab terminal
-`conda list --export > requirements.txt`
+`conda list --explicit > requirements.txt`
--- #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/run-jupyter.sh
+++ #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/run-jupyter.sh
@@ -5,6 +5,10 @@ if [ "$NUMBER" == "" ]; then
 	  NUMBER=0
 fi
 
-docker build -t "jupyter-$NUMBER" .
-docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/ "jupyter-$NUMBER"
+PORT="8888"
+RELATIVE_TO_GIT_ROOT=${PWD/"$(git rev-parse --show-toplevel)/"/}
+RELATIVE_TO_GIT_ROOT=${RELATIVE_TO_GIT_ROOT//#/%23}
+
+docker build -t "jupyter-$NUMBER" --build-arg MAPPED_PORT=$PORT --build-arg RELATIVE_PATH=$RELATIVE_TO_GIT_ROOT .
+docker run --rm -p $PORT:8888 -v "$(git rev-parse --show-toplevel)":/home/jovyan/ "jupyter-$NUMBER"
 
