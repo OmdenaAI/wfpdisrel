d4507d4c3195ca015262906b42b47e778d26cddc
Krzysztof Augustowski <krzysztof@augustowski.net>
2020-04-19 16:13:28 +0200

    jupyter in docker batch file in progress

--- #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/run-jupyter.bat
+++ #task1-datacollection/#task1-datacollection-climada/notebooks/notebook_folder_template/run-jupyter.bat
@@ -1,5 +1,11 @@
-set /a NUMBER=(%RANDOM%*max/32768)+min
+set /a NUMBER=(%RANDOM%*9999/32768)+1000
 
-docker build -t "jupyter-$NUMBER" .
-docker run --rm -p 8888:8888 -v "$PWD":/home/jovyan/ "jupyter-$NUMBER"
+set PORT="8888"
+for /f w in ('git rev-parse --show-toplevel') do set RELATIVE_TO_GIT_ROOT=%cd:w=""%}
+
+set RELATIVE_TO_GIT_ROOT=${RELATIVE_TO_GIT_ROOT//#/%23}
+
+docker build -t "jupyter-%NUMBER%" --build-arg MAPPED_PORT=%PORT% --build-arg RELATIVE_PATH=%RELATIVE_TO_GIT_ROOT% .
+docker run --rm -p %PORT%:8888 -v "$(git rev-parse --show-toplevel)":/home/jovyan/ "jupyter-%NUMBER%"
+
 
